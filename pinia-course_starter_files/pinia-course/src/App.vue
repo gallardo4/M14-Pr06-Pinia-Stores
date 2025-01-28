f7b77560a8805d91aebd9bf8e6bf3587a87cd50b
gallardo4 <arnau.gallardo.7e7@itb.cat>
2025-01-28 20:00:09 +0100

    Commit

---
+++ pinia-course_starter_files/pinia-course/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import TheHeader from "@/components/TheHeader.vue";
+import ProductCard from "@/components/ProductCard.vue";
+import { useProductStore } from "./stores/useProductStore";
+import { useCartStore } from "./stores/useCartStore";
+
+const productStore = useProductStore();
+const cartStore = useCartStore();
+
+productStore.fill();
+</script>
+
+<template>
+  <div class="container">
+    <TheHeader />
+    <ul class="sm:flex flex-wrap lg:flex-nowrap gap-5">
+      <ProductCard
+        v-for="product in productStore.products"
+        :key="product.name"
+        :product="product"
+        @addToCart="addToCart($event, product)"
+      />
+    </ul>
+  </div>
+</template>
---
+++ pinia-course_starter_files/pinia-course/src/stores/useCartStore.js
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+
+export const useCartStore = defineStore("CartStore", {
+    // per saber quins productes afegim al carrito
+    state: () => {
+        return {
+            items: [],
+        }
+    },
+    
+    actions: () => {
+        const addToCart = (count, product) => {
+            count = parseInt(count)
+            for (let index = 0; index < count; index++) {
+                useCartStore.items.push(product);
+            }
+        }
+    }
+});
